19ebd27216ffcd597d20947025c5590f2149e172
Melnychenko-Volodymyr <mvi_1968@i.ua>
2023-04-03 15:34:57 +0300

    change

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
-const http = require('http')
-const fs = require('fs')
+const http = require('http');
+const fs = require('fs');
+
+// глобальна заміна
+String.prototype.replaceAll = function(search, replacement)  {
+    let target = this;
+    return target.split(search).join(replacement);
+};
 
 http.createServer((request, response) => {
-    console.log(`Запит адреси: ${request.url}`)
-    const filePath = request.url.substr(1)
+    // заміна '%20' на ' ' в імені файла
+    const path = request.url.replaceAll('%20',' ');  
+    console.log(`Запит адреси: ${path}`);
+    const filePath = path.substr(1);
     // перевірка наявності файлу
     fs.access(filePath, fs.constants.R_OK, (err) => {
       // файл не знайдено
       if (err) {
-        response.statusCode = 404
-        response.end('Файл не знайдено!')
+        response.statusCode = 404;
+        response.end('Файл не знайдено!');
       } else {
-        fs.createReadStream(filePath).pipe(response)
+	// передаємо файл браузеру
+        fs.createReadStream(filePath).pipe(response);
       }
     })
   })
   .listen(3000, () => {
-    console.log('Старт сервера')
-  })
+    console.log('Старт сервера');
+  });
