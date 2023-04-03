f34e0ed9f4055852dc0be535c6a5a20fc2bd168c
Melnychenko-Volodymyr <mvi_1968@i.ua>
2023-04-03 23:36:10 +0300

    verify path

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
+
+// папка для файлів сайту на сервері
+const folderPath = '/site';
 
 // глобальна заміна
 String.prototype.replaceAll = function(search, replacement)  {
@@ -9,21 +13,33 @@ String.prototype.replaceAll = function(search, replacement)  {
 
 http.createServer((request, response) => {
     // заміна '%20' на ' ' в імені файла
-    const path = request.url.replaceAll('%20',' ');  
-    console.log(`Запит адреси: ${path}`);
-    const filePath = path.substr(1);
+    const requestPath = request.url.replaceAll('%20',' ');  
+    console.log(`Запит адреси: ${requestPath}`);
+    // перевірка наявності файла в папці folderPath
+    const relativePath = path.relative(folderPath, requestPath);
+    const isInsideFolder = relativePath && !relativePath.startsWith('..') && !path.isAbsolute(relativePath);
+
+if (isInsideFolder) {
+
+    const filePath = requestPath.substr(1);
     // перевірка наявності файлу
     fs.readFile(filePath, (error, data) => {
       // файл не знайдено
       if (error) {
         response.statusCode = 404;
+        response.setHeader('Content-Type', 'text/plain; charset=utf-8');
         response.end('Файл не знайдено!');
       } else {
 	// передаємо файл браузеру
         response.end(data);
       }
     })
-  })
-  .listen(3000, () => {
-    console.log('Старт сервера');
+  }  else {
+	response.statusCode = 403;
+	response.setHeader('Content-Type', 'text/plain; charset=utf-8');
+        response.end('Нєфіг шастать!');
+   }
+})
+.listen(3000, () => {
+   console.log('Старт сервера');
   });
